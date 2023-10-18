be9fcb1cdb046021ac3a3614327e79fe298a6773
Prof-Greipl <dieter.greipl@haw-landshut.de>
2023-10-18 14:10:57 +0200

    nach T2 (18.10.2023) - Menue erzeugt

--- app/src/main/java/de/hawlandshut/pluto24/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto24/MainActivity.java
@@ -1,14 +1,67 @@
 package de.hawlandshut.pluto24;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG ="xx MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "called onCreate");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "called onStart");
+        Toast.makeText(getApplicationContext(), "in onStart!!.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "called onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO: remove later!
+        super.onPause();
+        Log.d(TAG, "called onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "called onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "called onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "called onRestart");
     }
 }
